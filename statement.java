9ae6d0ddf6370b99206738ef065e5b2940793713
OJO RACHEAL OLUWAFUNMILAYO <55698184+Rizzy22@users.noreply.github.com>
2022-03-01 17:49:18 +0100

    Add files via upload
    statement in java

---
+++ statement.java
@@ -0,0 +1,16 @@
+public class statement{
+
+    public static void main(string [] args){
+        int boys = 20;
+        int girls = 50;
+    }
+    public static topic(){ 
+        system.out.println("There are seventy students in class")
+    }
+    public static total(){
+        if (boys < 10)
+        system.out.println("Yes")
+    }else{
+        system.out.println("No")
+    }
+}
